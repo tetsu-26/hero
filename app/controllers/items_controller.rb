daf384417c72cad28454ef8b9c7cf3be27dc1626
tetsu-26 <heiwa326427@gmial.com>
2019-09-09 10:43:34 +0900

    itembox表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-
+    @items = Item.all
   end
 
   def new
@@ -9,7 +9,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
